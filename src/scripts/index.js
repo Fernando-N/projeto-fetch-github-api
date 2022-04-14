e34614c6b4fa1843ce1d94948f8f9ec6a7a9f626
Fernando Nazario <fernandonazario4@gmail.com>
2022-04-13 23:59:58 -0300

    Refactoring

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,9 +1,20 @@
-import { user } from '/src/scripts/services/user.js '
-import { repos } from '/src/scripts/services/repositories.js '
+import { getUser } from '/src/scripts/services/user.js'
+import { getRepositories } from '/src/scripts/services/repositories.js'
+
+import { user } from '/src/scripts/objects/user.js'
+import { screen } from '/src/scripts/objects/screen.js'
+
+function validateEmptyInput(userName) {
+  if (userName.length === 0) {
+    alert('Preencha o campo com o nome do usuário do GitHub')
+    return true
+  }
+}
 
 document.getElementById('btn-search').addEventListener('click', () => {
   const userName = document.getElementById('input-search').value
-  getUserProfile(userName)
+  if (validateEmptyInput(userName)) return
+  getUserData(userName)
 })
 
 document.getElementById('input-search').addEventListener('keyup', e => {
@@ -11,39 +22,22 @@ document.getElementById('input-search').addEventListener('keyup', e => {
   const key = e.which || e.keyCode
   const isEnterKeyPressed = key === 13
   if (isEnterKeyPressed) {
-    getUserProfile(userName)
+    if (validateEmptyInput(userName)) return
+    getUserData(userName)
   }
 })
 
-function getUserProfile(userName) {
-  user(userName).then(userData => {
-    let userInfo = `
-      <div class="info">
-        <img src="${userData.avatar_url}" alt="Foto do perfil do usuário" />
-        <div class="data">
-          <h1>${userData.name ?? 'Não possui nome cadastrado'}</h1>
-          <p>${userData.bio ?? 'Não possui bio cadastrada'}</p>
-        </div>
-      </div>
-    `
-    document.querySelector('.profile-data').innerHTML = userInfo
-    getUserRepositories(userName)
-  })
-}
+async function getUserData(userName) {
+  const userResponse = await getUser(userName)
 
-function getUserRepositories(userName) {
-  repos(userName).then(reposData => {
-    let repositoriesItens = ''
+  if (userResponse.message === 'Not Found') {
+    screen.renderNotFound()
+    return
+  }
 
-    reposData.forEach(repo => {
-      repositoriesItens += `<li><a href="${repo.html_url}" target="_blank">${repo.name}</a></li>`
-    })
+  const repositoriesResponse = await getRepositories(userName)
+  user.setInfo(userResponse)
+  user.setRepositories(repositoriesResponse)
 
-    document.querySelector('.profile-data').innerHTML += `
-      <div class="repositories section">
-        <h2>Repositórios</h2>
-        <ul>${repositoriesItens}</ul>
-      </div>
-    `
-  })
+  screen.renderUser(user)
 }
